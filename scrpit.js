6ebc647efc61070d0d9dabb26f2ca007586a5d4a
Sabir <sabirn538@gmail.com>
2023-08-31 19:58:27 +0530

    adding  seconad question

--- scrpit.js
+++ scrpit.js
@@ -9,5 +9,16 @@ const questions = [
             
         ]
     },
+
+    {
+        question:"Which is the smallest country in the world? ",
+        answers:[
+            {text:"Vatican City", correct: true},
+            {text:"Bhutan", correct: false},
+            {text:"Nepal", correct: false},
+            {text:"Shri Lanka", correct: false},
+            
+        ]
+    },
     
 ]
