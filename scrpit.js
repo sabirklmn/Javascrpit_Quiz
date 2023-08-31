bd5f6f5c0d0f171e1eaa07e6ac5e94acb56003cc
Sabir <sabirn538@gmail.com>
2023-08-31 19:52:56 +0530

    adding style.js and question creating first

---
+++ scrpit.js
@@ -0,0 +1,13 @@
+const questions = [
+    {
+        question:"Which is largest animal in the world ",
+        answers:[
+            {text:"Shark", correct: false},
+            {text:"Blue whale", correct: true},
+            {text:"Elephnat", correct: false},
+            {text:"Giraffe", correct: false},
+            
+        ]
+    },
+    
+]
