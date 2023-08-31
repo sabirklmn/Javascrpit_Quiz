53bbf10baa08c09ecc3c233330565209b13c81f2
Sabir <sabirn538@gmail.com>
2023-08-31 20:03:34 +0530

    adding  3rd and 4th  question

---
+++ scrpit.js
@@ -0,0 +1,49 @@
+const questions = [
+    {
+        question:"Which is largest animal in the world ",
+        answers:[
+            {text:"Shark", correct: false},
+            {text:"Blue whale", correct: true},
+            {text:"Elephnat", correct: false},
+            {text:"Giraffe", correct: false},
+            
+        ]
+    },
+
+    {
+        question:"Which is the smallest country in the world? ",
+        answers:[
+            {text:"Vatican City", correct: true},
+            {text:"Bhutan", correct: false},
+            {text:"Nepal", correct: false},
+            {text:"Shri Lanka", correct: false},
+            
+        ]
+    },
+
+    {
+        question:"Which is largest desert in the world? ",
+        answers:[
+            {text:"Kalahari", correct: false},
+            {text:"Gobi", correct: false},
+            {text:"Sahara", correct: false},
+            {text:"Antarctica", correct: true},
+            
+        ]
+    },
+    
+
+
+    {
+        question:"Which is smallest contient in the world ",
+        answers:[
+            {text:"Asia", correct: false},
+            {text:"Austraila", correct: true},
+            {text:"Arctic", correct: false},
+            {text:"Africa", correct: false},
+            
+        ]
+    },
+    
+    
+];
